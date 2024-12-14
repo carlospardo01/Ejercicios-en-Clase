c14ce3978e7e9423cc98484bdaddef2eb791b054
Carlos Pardo <39810659+carlospardo01@users.noreply.github.com>
2024-12-14 12:13:56 -0500

    Add files via upload

--- 14-12-2024 Ejercicio While.py
+++ 14-12-2024 Ejercicio While.py
@@ -10,15 +10,21 @@
 #else:
 #    print("Lo siento, perdiste")
 
-
-Palabra = "Trasnocharse"
-contador=0
-limite=3
-while contador<limite:
-    respuesta = input("Adivinanza:\n¿Qué es lo que se hace de noche, que no se puede hacer de día?\n")
+#Lista con las respuestas correctas
+Palabra = ["Trasnocharse", "trasnocharse"]
+#Contador y límite de intentos
+contador = 0
+limite = 3
+#se usa la función While para contar la cantidad de intentos y ver si es correcto lo que responde
+while contador < limite:
+    # Se hace la pregunta de la adivinanza, se captrua el dato y se eliminan los espacios en blanco
+    respuesta = input("Adivinanza:\n¿Qué es lo que se hace de noche, que no se puede hacer de día?\n").strip()
+    #Cuenta la cantidad de respuestas
     contador += 1
-    if respuesta == Palabra:
+    #compara si la respuesta es correcta y si es correcta, imprime un mensaje en pantalla y rompe el bucle, de lo contrario vuelve a intentarlo hasta que acaba el bucle
+    if respuesta in Palabra:  # Compara si la respuesta está en la lista
         print("La respuesta es correcta.")
         break
+#Imprime mensje si no logro contestar en los intentos permitidos
 else:
-    print("Lo siento, Sigue intentando, la respuesta era Trasnocharse.")
+    print("Lo siento, sigue intentando, la respuesta es: Trasnocharse")
